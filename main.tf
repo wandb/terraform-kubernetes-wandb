9c837b50c014f6c0ba311d7c6989de21f9d5cb48
Ethan Ho <ethan.ho@wandb.com>
2022-07-13 13:29:39 -0700

    change sidecar tail directory

--- main.tf
+++ main.tf
@@ -160,7 +160,7 @@ resource "kubernetes_deployment" "wandb" {
           name = "sidecar"
           image = "busybox"
 
-          args = [ "/bin/sh", "-c", "tail -n+1 -f /var/log/gorilla.log" ]
+          args = [ "/bin/sh", "-c", "tail -n+1 -f pvc/var/log/gorilla.log" ]
           # name = "fluentd"
           # image = "fluent/fluentd-kubernetes-daemonset:v1-debian-elasticsearch"
 
