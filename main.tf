3567c6a20b2760ba28e62be7ec373f0e68dcb7a6
George Scott <george.scott@wandb.com>
2023-08-22 15:26:53 -0500

    mv services

--- main.tf
+++ main.tf
@@ -217,38 +217,6 @@ lifecycle {
   }
 }
 
-resource "kubernetes_service" "service" {
-  metadata {
-    name = "wandb"
-  }
 
-  spec {
-    type = "NodePort"
-    selector = {
-      app = "wandb"
-    }
-    port {
-      name      = "http"
-      port      = 8080
-      node_port = var.service_port
-    }
-  }
-}
-
-resource "kubernetes_service" "prometheus" {
-  metadata {
-    name = "prometheus"
-  }
-
-  spec {
-    selector = {
-      app = "wandb"
-    }
-    port {
-      name = "prometheus"
-      port = 8181
-    }
-  }
-}
 
 
