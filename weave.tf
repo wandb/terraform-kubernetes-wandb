ebdb2f9d5bade0180f00f0a1f50ec5c0b11ea3d5
Yogesh Garg <yogeshg91@gmail.com>
2023-08-23 14:29:14 -0700

    Revert "fix: Add security context to pvc. (#37)"
    This reverts commit f1053b70d0223e85b9252e8663aa42d84cfa7d81.

--- weave.tf
+++ weave.tf
@@ -136,11 +136,6 @@ resource "kubernetes_deployment" "weave" {
             }
           }
         }
-        securityContext {
-          fsGroup = 0
-          runAsUser = 999
-          fsGroupChangePolicy = "OnRootMismatch"
-        }
       }
     }
   }
